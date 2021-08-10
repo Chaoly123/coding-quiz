731af57815cdbd00a48b13a5caef41cef60fe6d1
Chao Ly <lychaos123@gmail.com>
2021-08-09 23:59:06 -0500

    change the coding to make it more easily to understand.

--- java.js
+++ java.js
@@ -1,32 +1,27 @@
-var highscores = [];
 
-var timerElement = document.querySelector(".timer-count");
-var startButton = document.querySelector(".start-button");
-var testBtn = document.querySelector("#btn")
+var time;
+var secoundLeft = 60;
+var timerCount;
 
-var viewScores = document.getElementById("View-scores");
+var question = "";
+var answer = "";
+var highscore = 0;
 
-var beginHere = document.getElementById("begin-here");
-var questionStart = document.getElementById("questionstart");
-var question = document.getElementById("question");
-var answer = document.getElementById("answer");
+let startButton = document.getElementById("start-button");
+let timeEl = document.querySelector(".time");
+let questionEl = document.querySelector("question");
+let answerEl = document.querySelector("correct-answers");
+let correctEl = "";
+let currentQuestion = question.length - 1;
+let highscoreEl = document.querySelector("view-scores");
 
-var enterName = document.getElementById("enter-name");
-var display = document.getElementById("display");
-var initial = document.getElementById("initial");
-var initialSubmit = document.getElementById("initial-submit");
-
-var highscore = document.getElementById("highscore");
-var highscoreBox = document.getElementById("highscore-box");
-var back = document.getElementById("back");
-var clear = document.getElementById("clear");
-
-var pageArray = [beginHere, questionStart, question, initial, highscore];
-
-var timer, timerCount, isWin;
+var allScore = [];
+// var storeScores = json.parse(localStorage.getItem("userData"));
+var question = 0;
 
 
-var questionContent = {
+var question = [
+
   question1 = {
     question: "What does NOT belong in HTML coding?",
     correct: "function",
@@ -55,109 +50,51 @@ var questionContent = {
   question6 = {
     question: "What does belong in java coding?",
     correct: "var",
-    answerArray: ["body", "header", "div", "fuction"]
-  },
-}
-
-
-
-function onclick() {
-  isWin = false;
-  timerCount = 60;
-  // Prevents start button from being clicked when round is in progress
-  startButton.disabled = true;
-  // renderBlanks()
-  startTimer()
-}
-
-function decreaseTime() {
-  timerCount -= 3
-}
-
-function setStateWin() {
-  isWin = true;
-}
-
-function startTimer() {
-  // Sets timer
-  // var test = setInterval(function() {
-  //   console.log("information")
-  // }, 1000);
-
-  timer = setInterval(function () {
-    timerCount--;
-    timerElement.textContent = timerCount;
-    if (timerCount >= 0) {
-      // Tests if win condition is met
-      // if (isWin && timerCount > 0) {
-      //   // Clears interval and stops timer
-      //   winGame();
-      // }
-    }
-
-    if (timerCount <= 0 || isWin) {
-      clearInterval(timer);
-      console.log("game ended")
-    }
-  }, 1000)
-}
-var q1, q2, q3, q4, q5, q6;
-// var arr = [null, q1, q2, q3, q4, q5, q6]
-var arrBtn = document.querySelectorAll(".saveBtn")
-arrBtn.forEach(el => {
-  el.addEventListener("click", saveInput)
-})
-
-function saveInput(e) {
-  e.preventDefault()
-  var context = document.querySelectorAll('input[name="choice' + this.value + '"]');
-  var selectedVal;
-  for (var element of context) {
-    // console.log(element)
-    if (element.checked) {
-      selectedVal = element.value;
-      break
-    }
+    answerArray: ["body", "header", "var", "fuction"]
   }
-  if (this.value == 1) {
-    q1 = selectedVal
-  } else if (this.value == 2) {
-    q2 = selectedVal
-  } else if (this.value == 3) {
-    q3 = selectedVal
-  } else if (this.value == 4) {
-    q4 = selectedVal
-  } else if (this.value == 5) {
-    q5 = selectedVal
-  } else if (this.value == 6) {
-    q6 = selectedVal
-  }
+
+];
+
+const totalPoints = 120;
+const totalQuestion = 6;
+
+
+function start() {
+  time()
+  getQuestion()
+  question()
 }
 
+function time() {
+  let timeInterval = setInterval(function() {
+    if( secoundLeft === 0 ) {
+      clearInterval(timeInterval);
+      timeEl.textContent = "Out of Time!!!!"
+    } else {
+      secoundLeft--;
+      timeEl.textContent = secoundLeft;
+    }
+  }, 1000);
+}
 
-function check() {
-  console.log(q1, q2, q3, q4, q5, q6)
-  var c = 0;
-  // var q1 = document.quiz.question1.value;
-  // var q2 = document.quiz.question2.value;
-  // var q3 = document.quiz.question3.value;
-  // var q4 = document.quiz.question4.value;
-  // var q5 = document.quiz.question5.value;
-  // var q6 = document.quiz.question6.value;
-  if (q1 == "function") { c++ }
-  if (q2 == "div") { c++ }
-  if (q3 == "header") { c++ }
-  if (q4 == "div") { c++ }
-  if (q5 == "background") { c++ }
-  if (q6 == "var") { c++ }
+function getQuestion () {
+  questionEl.append(question[question].question);
+  question[question].options.forEach(function (singleanswer) {
+    var rightButton = document.createElement("button");
+    rightButton.id = "rightButton";
+    rightButton.textContent = singleanswer;
+    rightEl.appendchild(rightButton);
+  });
+}
 
-  if (c == 6) {
-    setStateWin()
+function question() {
+  if( answerEl === question[question].correct) {
+    textContent = "You are right"
+    highscore++;
+    question++;
   } else {
-    decreaseTime()
+    time -= 3
   }
-  // document.write(ok);
-}
+};
 
-startButton.addEventListener("click", onclick)
-testBtn.addEventListener("click", decreaseTime)
+startButton.addEventListener("click", start)
